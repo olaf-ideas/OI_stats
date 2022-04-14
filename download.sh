e63ca49401d5767fad9219eeb2b5093119402dca
Olaf Surgut <olaf.ideas@gmail.com>
2022-04-14 17:23:48 +0200

    complete set of data

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-for ((numer = 29; numer >= 19; numer--)); do
+for ((numer = 29; numer >= 1; numer--)); do
 	for ((etap = 1; etap <= 3; etap++)); do
 		echo -n "DOWNLOADING OI${numer} etap${etap} $((numer+1993)): "
 		python3 fetch_data.py $numer $etap
